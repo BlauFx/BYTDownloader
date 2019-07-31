0231c6c95840d076d38599916aba0d9db16b670f
BlauFx <clanpower15@gmail.com>
2019-07-31 18:08:02 +0200

    Update Converter

--- BYTDownloader/Converter.cs
+++ BYTDownloader/Converter.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using MediaToolkit;
 using MediaToolkit.Model;
 using MediaToolkit.Options;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace BYTDownloader
 {
     class Converter
     {
+        private string TmpTitle = string.Empty;
+
+        private int Files = 0;
+        
         public Converter()
         {
             Console.Title = "Converter";
@@ -21,7 +29,9 @@ namespace BYTDownloader
 
             Console.Write("Fomat: ");
 
-            var outputFile = new MediaFile { Filename = string.Format(@"{0}\output.{1}", Environment.GetFolderPath(Environment.SpecialFolder.Desktop), Console.ReadLine()) };
+            var format = Console.ReadLine();
+
+            var outputFile = new MediaFile { Filename = string.Format(@"{0}\{1}.{2}", GetFileDir(x), GetOutputName(x,format), format) };
 
             var conversionOptions = new ConversionOptions
             {
@@ -40,9 +50,8 @@ namespace BYTDownloader
 
             if (File.Exists(outputFile.Filename))
             {
-                Console.WriteLine("100%");
-                Console.WriteLine("--------------------------------------------------");
-                Console.WriteLine("--------------------------------------------------");
+                System.Threading.Thread.Sleep(100);
+                Console.WriteLine("Done");
             }
             else
             {
@@ -51,7 +60,104 @@ namespace BYTDownloader
             }
         }
 
-        private static void Engine_ConvertProgressEvent(object sender, ConvertProgressEventArgs e)
+        private string GetFileDir(string inputFile)
+        {
+            int num = inputFile.LastIndexOf("\\", StringComparison.Ordinal);
+            string str = inputFile.Substring(0, num);
+
+            return str;
+        }
+
+        private string GetOutputName(string inputFile, string format)
+        {
+            int num = inputFile.LastIndexOf("\\", StringComparison.Ordinal)+1;
+            int num2 = inputFile.LastIndexOf(".", StringComparison.Ordinal);
+            string str = inputFile.Substring(num,  num2 - num);
+            
+            string checkEng = EngAlphabet(str);
+
+            Format fmr = parseFormat(format);
+            
+            string finalstr = CheckIfAvailableName(GetFileDir(inputFile), checkEng, fmr);
+            return finalstr;
+        }
+        
+        private string EngAlphabet(string tit)
+        {
+            List<string> tmp = new List<string>();
+            string Titel = string.Empty;
+            
+            foreach (var x in tit)
+            {
+                bool result = x.ToString().Any(x => char.IsLetterOrDigit(x));
+                bool result2 = x.ToString().Any(x => char.IsWhiteSpace(x));
+
+                if (result || result2)
+                {
+                    tmp.Add(x.ToString());
+                }
+                else
+                {
+                    tmp.Add("#");
+                }
+            }
+            
+            foreach (var x in tmp)
+            {
+                Titel += x;
+            }
+            
+            return Titel;
+        }
+
+        private string CheckIfAvailableName(string path, string name, Format format = Format.Mp3)
+        {
+            if (File.Exists(path + $"\\{name}.{format}"))
+            {
+                CheckIfAvailableNameVoid(path, name, format);
+                return TmpTitle;
+            }
+            return name;
+        }
+
+        private void CheckIfAvailableNameVoid(string path, string name, Format format = Format.Mp3)
+        {
+            if (File.Exists(path + $"\\{name}.{format}"))
+            {
+                Files++;
+                CheckIfAvailableNameVoid(path, FileCutter(name,Files), format);
+            }
+            else
+            {
+                Files = 0;
+                TmpTitle = name;
+            }
+        }
+
+        private string FileCutter(string str, int num)
+        {
+            if (str.Contains("("))
+            {
+                int i = str.IndexOf("(", StringComparison.Ordinal);
+                string j = str.Substring(0, i);
+                return $"{j} ({num})";
+            }
+            
+            return $"{str} ({num})";
+        }
+        
+        private Format parseFormat(string format)
+        {
+            if (!(String.IsNullOrEmpty(format)))
+            {
+                Format dir = (Format)Enum.Parse(typeof(Format), format,true);
+                return dir;
+            }
+            throw new NullReferenceException("string can not be null");
+        }
+
+
+        private void Engine_ConvertProgressEvent(object sender, ConvertProgressEventArgs e)
         {
             Console.ForegroundColor = ConsoleColor.Red;
 
--- BYTDownloader/Program.cs
+++ BYTDownloader/Program.cs
@@ -119,5 +119,6 @@ namespace BYTDownloader
     {
         Mp3,
         Mp4,
+        wav,
     }
 }
