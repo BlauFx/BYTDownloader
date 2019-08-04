d4efba5bd55ce5fd0c8bc1a560506f67bfd0a028
BlauFx <clanpower15@gmail.com>
2019-08-04 22:55:55 +0200

    Add Queue

---
+++ BYTDownloader/Queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BYTDownloader
+{
+    class Queue
+    {
+        public Queue()
+        {
+            Console.WriteLine("Add links to the queue!");
+            Console.WriteLine();
+        }
+    }
+}
