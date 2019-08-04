11f568bc63de17baaaf33472762b8adbdececd35
BlauFx <clanpower15@gmail.com>
2019-08-05 00:03:03 +0200

    Add DownloadQueue

--- BYTDownloader/Queue.cs
+++ BYTDownloader/Queue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BYTDownloader
 {
@@ -7,7 +8,28 @@ namespace BYTDownloader
         public Queue()
         {
             Console.WriteLine("Add links to the queue!");
-            Console.WriteLine();
+            Console.WriteLine("If you're done than type: Done");
+            
+            List<string> tmp = new List<string>();
+
+            while (true)
+            {
+                string y = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(y))
+                {
+                    if (y.Contains("Done"))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        tmp.Add(y);
+                    }
+                }
+            }
+
+            new DownloadQueue(tmp);
         }
     }
 }
